ce00ee8bdcfbf70dafbce098995ff7b3b1904210
Annastasshia Ames <aames26@gmail.com>
2020-08-25 03:01:23 -0400

    fixed input/button functionality

---
+++ script.js
@@ -0,0 +1,104 @@
+
+document.getElementById("6button").addEventListener('click', function(event) { 
+    event.preventDefault();
+    console.log("Ive been clicked")
+    textValue= $("#6am").val()
+    $("#6amAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("7button").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#7am").val()
+    $("#7amAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("8button").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#8am").val()
+    $("#8amAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("9button").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#9am").val()
+    $("#9amAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("10button").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#10am").val()
+    $("#10amAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("11button").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#11am").val()
+    $("#11amAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("12pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#12pm").val()
+    $("#12pmAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("1pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#1pm").val()
+    $("#1pmAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("2pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#2pm").val()
+    $("#2pmAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("3pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#3pm").val()
+    $("#3pmAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("4pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#4pm").val()
+    $("#4pmAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("5pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#5pm").val()
+    $("#5pmAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("6pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#6pm").val()
+    $("#6pmAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("7pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#7pm").val()
+    $("#7pmAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("8pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#8pm").val()
+    $("#8pmAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("9pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#9pm").val()
+    $("#9pmAdd").append("<li>"+textValue+"</li>");
+  });
+
+  document.getElementById("10pbutton").addEventListener('click', function(event) { 
+    event.preventDefault();
+    textValue= $("#10pm").val()
+    $("#10pmAdd").append("<li>"+textValue+"</li>");
+  });
+
